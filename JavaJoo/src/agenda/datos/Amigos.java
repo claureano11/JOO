e6cac907b7678a3d798e48882c4b27d22a088236
DW31 <ik012982i9@H012982.NET>
2016-10-14 09:53:12 +0200

    java archivos

---
+++ JavaJoo/src/agenda/datos/Amigos.java
@@ -0,0 +1,42 @@
+package agenda.datos;
+
+public class Amigos {
+	private String nombre;
+	private String email;
+	private String telefono;
+	
+	//declarar metodo constructor
+	public Amigos(String nombre){
+		
+	}
+
+	//metodos getters and setters
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	
+	
+}
---
+++ JavaJoo/src/zinemaldi2/gui/Programa.java
@@ -0,0 +1,26 @@
+package zinemaldi.gui;
+import java.util.Scanner;
+import zinemaldi.datos.Pelicula;
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Pelicula peli= new Pelicula("Mascotas");
+	
+		Scanner sc=new Scanner(System.in);
+		String titulo;
+		Pelicula peli;
+		//String director;
+		System.out.print("Titulo: ");
+		titulo=sc.nextLine();
+		peli=new Pelicula(titulo);
+		System.out.print("Nombre Director: ");
+		peli.setDirector(sc.nextLine());
+		System.out.print("Edad Director: ");
+		peli.setEdadDirector(sc.nextInt());
+	
+		System.out.print("Duracion: ");
+		peli.setDuracion(sc.nextInt());
+	}
+
+}
